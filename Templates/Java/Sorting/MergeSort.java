ddff79cedb55b9aa5c266014e4585a6888cc7e32
Rajeev <rajeev29five@gmail.com>
2018-09-30 11:10:06 +0530

    Sorting algo

---
+++ Templates/Java/Sorting/MergeSort.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+class MergeSort {
+
+    public static void merge(int a[], int l, int m, int n)
+    {
+        int n1 = m - l + 1;
+        int n2 = n - m;
+
+        int left[] = new int[n1];
+        int right[] = new int[n2];
+
+        for(int i = 0; i<n1; i++) left[i] = a[l+i];
+        for(int j = 0; j<n2; j++) right[j] = a[m+1+j];
+
+        int i = 0, j = 0, k = l;
+
+        while(i < n1 && j < n2) {
+
+            if(left[i] <= right[j]) {
+                a[k] = left[i];
+                i++;
+            }
+            else {
+                a[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < n1) {
+            a[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while(j < n2) {
+            a[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int a[], int l, int n) {
+
+        int m = (n+l)/2;
+        if(l<n)
+        {
+            mergeSort(a, l, m);
+            mergeSort(a, m+1, n);
+
+            merge(a, l, m, n);
+        }
+
+    }
+
+    public static void printArray(int a[]) {
+        for(int i : a) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i<n; i++) {
+            a[i] = sc.nextInt();
+        }
+        mergeSort(a, 0, n-1);
+        printArray(a);
+    }
+
+}
---
+++ Templates/Java/Sorting/QuickSort.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class QuickSort {
+
+    public static int partition(int a[], int l, int n) {
+        int pivot = a[n];
+        int i = l - 1;
+
+        for(int j = l; j<=n-1; j++) {
+            if(a[j]<=pivot) {
+                i = i + 1;
+                int temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+        }
+        int temp = a[n];
+        a[n] = a[i+1];
+        a[i+1] = temp;
+
+        return i+1;
+    }
+
+    public static void quickSort(int a[], int l, int n) {
+        if(l<n)
+        {
+            int pi = partition(a, l, n);
+
+            quickSort(a, l, pi-1);
+            quickSort(a, pi+1, n);
+        }
+    }
+
+    public static void printArray(int a[]) {
+        for(int i : a) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i<a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        quickSort(a, 0, n-1);
+        printArray(a);
+    }
+
+}
