57ad392d5921375856d6fb45f31adc270feac267
Rajeev <rajeev29five@gmail.com>
2019-03-10 00:09:25 +0530

    Added Delete Operations

--- Templates/C++/LinkedList/LinkedList.cpp
+++ Templates/C++/LinkedList/LinkedList.cpp
@@ -2,6 +2,7 @@
 // Code Contributor(s) : Rajeev Ranjan
 //
 
+
 #include "iostream"
 
 using namespace std;
@@ -17,25 +18,28 @@ public:
 class LinkedList
 {
 private:
-	Node * first;
-	Node * last;
+	Node * head;
+	Node * tail;
 	int length;
 	
 public:
 	LinkedList () {
-		first = NULL;
-		last = NULL;
+		head = NULL;
+		tail = NULL;
 		length = 0;
 	}
 	void insertFront(int data);
-	void insertLast(int data);
+	void inserttail(int data);
 	void insertPosition(int data, int position);
+	void deleteHead();
+	void deleteTail();
+	void deletePosition(int position);
 	void showList();
 	int getLength();
 };
 
 void LinkedList::showList() {
-	Node * curr = first;
+	Node * curr = head;
 	cout<<endl<<"Updated List"<<endl;
 	while(curr!=NULL) {
 		cout<<curr->data<<" ";
@@ -50,37 +54,37 @@ int LinkedList::getLength() {
 
 
 void LinkedList::insertFront(int data) {
-	if (first == NULL) {
+	if (head == NULL) {
 		Node * newNode = new Node;
 		newNode->data = data;
 		newNode->next = NULL;
-		first = newNode;
-		last = newNode;
+		head = newNode;
+		tail = newNode;
 	}
 	else {
 		Node * newNode = new Node;
 		newNode->data = data;
-		newNode->next = first;
-		first = newNode;
+		newNode->next = head;
+		head = newNode;
 	}
 	length++;
 	showList();
 }
 
-void LinkedList::insertLast(int data) {
-	if (last == NULL) {
+void LinkedList::inserttail(int data) {
+	if (tail == NULL) {
 		Node * newNode = new Node;
 		newNode->data = data;
 		newNode->next = NULL;
-		first = newNode;
-		last = newNode;
+		head = newNode;
+		tail = newNode;
 	}
 	else {
 		Node * newNode = new Node;
 		newNode->data = data;
 		newNode->next = NULL;
-		last->next = newNode;
-		last = newNode;
+		tail->next = newNode;
+		tail = newNode;
 	}
 	length++;
 	showList();
@@ -94,13 +98,13 @@ void LinkedList::insertPosition(int data, int position) {
 		insertFront(data);
 	}
 	else if (position == length) {
-		insertLast(data);
+		inserttail(data);
 	}
 	else if (position > length){
 		cout<<"Length of LinkedList is "<<getLength()<<" element cannot be inserted at "<<position<<endl;
 	}
 	else {
-		Node * temp = first;
+		Node * temp = head;
 		while(temp != NULL && counter != position) {
 			counter++;
 			temp = temp->next;
@@ -112,7 +116,67 @@ void LinkedList::insertPosition(int data, int position) {
 		length++;
 		showList();
 	}	
+}
 
+void LinkedList::deleteHead() {
+	if (head == tail) {
+		head = NULL;
+		tail = NULL;
+	}
+	else {
+		Node * temp = head;
+		head = temp->next;
+		delete temp;
+	}
+	length--;
+}
+
+void LinkedList::deleteTail() {
+
+	if (head == tail) {
+		head = NULL;
+		tail = NULL;
+	}
+	else {
+		Node * curr = head;
+		while (curr->next != tail) {
+			curr = curr->next;
+		}
+
+		tail = curr;
+		Node * temp = curr->next;
+		curr->next = NULL;
+		delete temp;
+	}
+	length--;
+}
+
+void LinkedList::deletePosition(int position) {
+
+	if (head == tail) {
+		head = NULL;
+		tail = NULL;
+		length--;
+	}
+	else if (position == getLength()-1) {
+		deleteTail();
+	}
+	else if (position == 0) {
+		deleteHead();
+	}
+	else {
+		Node * curr = new Node;
+		Node * previous = new Node;
+		curr = head;
+		for (int i = 0; i<position; i++){
+			previous = curr;
+			curr = curr->next;
+		}
+		previous->next = curr->next;
+		curr->next = NULL;
+		delete curr;
+		length--;
+	}
 }
 
 int main(int argc, char const *argv[])
@@ -127,6 +191,9 @@ int main(int argc, char const *argv[])
 		cout<<"      3. For Insertion at particular Position "<<endl;
 		cout<<"      4. See LinkedList "<<endl;
 		cout<<"      5. See Length of LinkedList "<<endl;
+		cout<<"      6. For Head Deletion "<<endl;
+		cout<<"      7. For Tail Deletion "<<endl;
+		cout<<"      8. For Deletion at particular Position "<<endl;
 		cin>>choice;
 		if (choice == 1) {
 
@@ -138,7 +205,7 @@ int main(int argc, char const *argv[])
 
 			cout<<"Enter data : ";
 			cin>>data;
-			l.insertLast(data);
+			l.inserttail(data);
 		}
 		else if (choice == 3) {
 
@@ -156,6 +223,36 @@ int main(int argc, char const *argv[])
 		}
 		else if (choice == 5) {
 			cout<<"Length : "<<l.getLength()<<endl;
+		}
+		else if (choice == 6) {
+			if (l.getLength() != 0) {
+				l.deleteHead();
+				l.showList();
+			} 
+			else {
+				cout<<"There are no element in LinkedList"<<endl;
+			}
+		}
+		else if (choice == 7) {
+			if (l.getLength() != 0) {
+				l.deleteTail();
+				l.showList();
+			}
+			else {
+				cout<<"There are no element in LinkedList"<<endl;
+			}
+		}
+		else if (choice == 8) {
+			int position;
+			cout<<"Enter position : "; 
+			cin>>position;
+			if (l.getLength() != 0) {
+				l.deletePosition(position);
+				l.showList();
+			}
+			else {
+				cout<<"Node cannot be deleted"<<endl;
+			}
 		}
 		else {
 			break;
